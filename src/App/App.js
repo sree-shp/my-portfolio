fba4f153e16b875ef56eaa1ad7c9a4be23d47454
Sreehari Prasad <sree2512k@gmail.com>
2023-01-12 13:18:35 +0530

    add title section

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar.js";
+import Title from "../Title/Title.js";
+import "./App.css";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Title />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Title/Title.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Title.css";
+import man from "../Title/man.png";
+
+function Title() {
+  return (
+    <div id="title">
+      <div className="welcome-page">
+        <h1 className="welcome-heading">HI,</h1>
+        <h2 className="welcome-heading">I'M SREEHARI</h2>
+        <p className="description-heading">FRONTEND DEVELOPER</p>
+        <p className="description">Creating classy websites is my passion</p>
+      </div>
+      <img className="home-img" src={man} alt="" />
+      <div className="background-elements">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
