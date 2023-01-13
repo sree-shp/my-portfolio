710fb9096f909b0ae963948ad02ce11fc750610e
Sreehari Prasad <sree2512k@gmail.com>
2023-01-13 10:33:42 +0530

    add hamburger menu for mobile and tablet

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,24 +1,73 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  var [state, setState] = useState(false, () => {
+    state = !state;
+  });
+
+  useEffect(() => {
+    const hamburgerIcon = document.getElementsByClassName(
+      "hamburger-icon-container"
+    );
+    const hamburgerWrap = document.getElementsByClassName("hamburger-wrap");
+
+    console.log(hamburgerIcon);
+
+    hamburgerIcon[0].addEventListener("click", function () {
+      setState();
+      hamburgerWrap[0].classList.toggle("active");
+    });
+  }, [state]);
+
   return (
     <div id="header">
-      <a className="home-link" href="contact">
-        SREEHARI
-      </a>
-      <a
-        className="link-text"
-        href="https://www.linkedin.com/in/sreehari-prasad-k-819a49235/"
-      >
-        LINKEDIN
-      </a>
-      <a className="link-text" href="https://github.com/sree-shp">
-        GITHUB
-      </a>
-      <a className="link-text" href="mailto:sree2512k@gmail.com">
-        EMAIL
-      </a>
+      <div className="header-wrap">
+        <a className="home-link" href="contact">
+          SREEHARI
+        </a>
+        <div className="hamburger-menu">
+          <div className="hamburger-icon-container">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+        </div>
+
+        <a
+          className="link-text"
+          href="https://www.linkedin.com/in/sreehari-prasad-k-819a49235/"
+        >
+          ABOUT
+        </a>
+        <a className="link-text" href="https://github.com/sree-shp">
+          PROJECTS
+        </a>
+        <a className="link-text" href="mailto:sree2512k@gmail.com">
+          CONTACT
+        </a>
+      </div>
+      <div className="hamburger-wrap active">
+        <div className="hamburger-item">
+          <a
+            className="hamburger-link"
+            href="https://www.linkedin.com/in/sreehari-prasad-k-819a49235/"
+          >
+            ABOUT
+          </a>
+        </div>
+        <div className="hamburger-item">
+          <a className="hamburger-link" href="https://github.com/sree-shp">
+            PROJECTS
+          </a>
+        </div>
+        <div className="hamburger-item">
+          <a className="hamburger-link" href="mailto:sree2512k@gmail.com">
+            CONTACT
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
